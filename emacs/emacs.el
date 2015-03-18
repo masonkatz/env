ceabcebc32b5a21123fdf2a4e63b6287310f7227
Mason J. Katz <mason.katz@StackIQ.com>
2015-03-18 16:37:10 -0700

    checkpoint
    no idea — been too long

--- emacs/emacs.el
+++ emacs/emacs.el
@@ -3,10 +3,12 @@
 (setq load-path (cons "~/emacs" load-path))
 (setq load-path (cons "~/emacs/org-7.4/lisp" load-path))
 
+(if (display-graphic-p)
+    (progn
+      (require 'zenburn)
+      (zenburn)
+      (tool-bar-mode 0)))
 
-(cond ((eq window-system 'x)
-       '(lambda ()
-	  (require 'zenburn))))
 
 ;; mac keyboard
 (global-set-key "\M-[h" (lambda () (interactive) (beginning-of-line 'nil)))
@@ -35,8 +37,9 @@
 - turn on hideshow-org (org-mode like folding)
 "
   (interactive)
-  (cond ((eq window-system 'x)
-	 (linum-mode 1)))
+  (if (display-graphic-p)
+      (progn
+	(linum-mode 1)))
   (column-marker-1 80))
 
 
@@ -66,8 +69,8 @@
 
 (add-hook 'python-mode-hook
 	  '(lambda ()
-	     (setq py-smart-indentation nil)
 	     (setq py-indent-offset 8)
+	     (setq py-indent-paren-spanned-multilines-p 't)
 	     (setq python-indent 8)
 	     (setq tab-width 8)
 	     (mjk-code-mode)))
@@ -91,75 +94,4 @@
 	     (mjk-code-mode)))
 
 
-
-;;
-;; org-mode settings
-;;
-
-(global-set-key "\C-cl" 'org-store-link)
-(global-set-key "\C-cc" 'org-capture)
-(global-set-key "\C-ca" 'org-agenda)
-(global-set-key "\C-cb" 'org-iswitchb)
-
-
-(org-babel-do-load-languages 'org-babel-load-languages
-			     '((python . t)
-			       (dot . t)
-			       (emacs-lisp . t)))
-
-
-
-(setq org-agenda-include-diary t)
-
-(setq org-agenda-custom-commands
-      '(("X" agenda "" 	 nil ("~/Desktop/Critical/AGENDA.html"))
-	("Y" todo   ""   nil ("~/Desktop/Eventual/TASKS.html"))
-	))
-
-
-(setq org-capture-templates
-     '(("j" "Journal" entry (file+datetree "~/org/journal.org")
-	"* %?\n  %i")
-      ("t" "Todo" entry (file+headline "~/org/tasks.org" "InBox")
-	"* TODO %?\n  %i")))
-
-
-(add-hook 'org-mode-hook 
-	  '(lambda ()
-	     (setq org-highest-priority ?1
-		   org-default-priority ?2
-		   org-lowest-priority ?3)
-	     (setq org-src-fontify-natively t)
-	     (setq org-hide-leading-stars t)
-	     (setq org-hidden-keywords t)
-	     (setq org-log-done 'time)
-	     (setq org-enforce-todo-dependencies t)
-	     (setq org-enforce-todo-checkbox-dependencies t)
-	     (setq org-todo-keywords 
-		   '((sequence "TODO(t)"  "STARTED(s)" "BLOCKED(b@)"  "|"
-			       "DONE(d)" "CANCELED(x)" "DELEGATED(o@)")))
-	     (setq org-tag-alist '((:startgroup . nil)
-				   ("@errand" . ?E)
-				   ("@home" . ?H)
-				   ("@office" . ?O)
-				   ("@phone" . ?P)
-				   (:endgroup . nil)
-				   ("admin" . ?a)
-				   ("amazon" . ?A)
-				   ("buy" . ?b) 
-				   ("clustercorp" . ?c)
-				   ("dell" . ?d)
-				   ("family" . ?f)
-				   ("hp" . ?h)
-				   ("kayo" . ?k)
-				   ("personal" . ?p)
-				   ("rocks+" . ?r)
-				   ("rocks" . ?R)
-				   ("ucsd" . ?u)))
-	     (setq org-agenda-files (list
-				     "~/org/tasks.org"
-				     "~/org/journal.org"
-				     "~/org/someday.org"))
-	     (setq org-mobile-directory "~/Dropbox/MobileOrg")
-	     (turn-on-iimage-mode)))
 
--- profile
+++ profile
@@ -11,3 +11,12 @@ fi
 
 source ~/.bashrc
 
+
+##
+# Your previous /Users/mjk/.profile file was backed up as /Users/mjk/.profile.macports-saved_2014-12-16_at_11:52:27
+##
+
+# MacPorts Installer addition on 2014-12-16_at_11:52:27: adding an appropriate PATH variable for use with MacPorts.
+export PATH="/opt/local/bin:/opt/local/sbin:$PATH"
+# Finished adapting your PATH environment variable for use with MacPorts.
+
